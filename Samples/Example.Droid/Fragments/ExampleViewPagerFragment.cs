47094d347abab1030e627f265165b10c81a616c5
texas16 <ilyasuyanik@gmail.com>
2016-03-14 17:37:30 -0500

    fixing bug

--- Samples/Example.Droid/Fragments/ExampleViewPagerFragment.cs
+++ Samples/Example.Droid/Fragments/ExampleViewPagerFragment.cs
@@ -24,21 +24,14 @@ namespace Example.Droid.Fragments
             if (viewPager != null)
             {
 
-                var fragments = new List<MvxFragmentPagerAdapter.FragmentInfo>
-                {
-					
-                    new MvxFragmentPagerAdapter.FragmentInfo("RecyclerView 1", typeof (RecyclerViewFragment),
-						typeof (RecyclerViewModel)),
-                    new MvxFragmentPagerAdapter.FragmentInfo("RecyclerView 2", typeof (RecyclerViewFragment),
-                        typeof (RecyclerViewModel)),
-                    new MvxFragmentPagerAdapter.FragmentInfo("RecyclerView 3", typeof (RecyclerViewFragment),
-                        typeof (RecyclerViewModel)),
-                    new MvxFragmentPagerAdapter.FragmentInfo("RecyclerView 4", typeof (RecyclerViewFragment),
-                        typeof (RecyclerViewModel)),
-                    new MvxFragmentPagerAdapter.FragmentInfo("RecyclerView 5", typeof (RecyclerViewFragment),
-                        typeof (RecyclerViewModel))
-                };
-                viewPager.Adapter = new MvxFragmentPagerAdapter(Activity, ChildFragmentManager, fragments);
+				var vm = new RecyclerViewModel();
+				var fragments = new List<MvxFragmentPagerAdapter.FragmentInfo>();
+				for(int i = 0; i < vm.Items.Count; i++)
+				{
+					fragments.Add(new MvxFragmentPagerAdapter.FragmentInfo("RecyclerView " + (i+1).ToString(), typeof (RecyclerViewFragment),typeof (RecyclerViewModel)));
+				}
+
+				viewPager.Adapter = new MvxFragmentPagerAdapter(Activity, ChildFragmentManager, fragments);
             }
 
             var tabLayout = view.FindViewById<TabLayout>(Resource.Id.tabs);
